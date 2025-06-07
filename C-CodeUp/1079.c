cec6e57d97e2cc2059d76da31fa1dda358c2f880
박성준 <freestarjun@gmail.com>
2025-06-07 23:41:22 +0900

    2025/6/7/(토)

--- C-CodeUp/1079.c
+++ C-CodeUp/1079.c
@@ -1,1 +1,14 @@
-//
+// 원하는 문자가 입력될 때까지 반복 출력하기
+
+#include <stdio.h>
+int main(void)  {
+    char a;
+    while (1) {
+        scanf("%c", &a);
+        printf("%c\n", a);
+        if(a == 'q') {
+            break;
+        }
+    }
+    return 0;
+}
