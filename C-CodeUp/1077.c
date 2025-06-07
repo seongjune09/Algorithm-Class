7e3865497752c4db554016d5d567dc440bd3e83b
박성준 <freestarjun@gmail.com>
2025-06-07 23:28:30 +0900

    2025/6/7/(토)

--- C-CodeUp/1077.c
+++ C-CodeUp/1077.c
@@ -1,1 +1,12 @@
-//
+// 정수 1개 입력받아 그 수까지 출력하기
+
+#include <stdio.h>
+int main(void)  {
+    int n,i;
+    scanf("%d", &n);
+    for(int i=0; i<=n; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
--- C-CodeUp/1078.c
+++ C-CodeUp/1078.c
@@ -1,1 +1,15 @@
-//
+// 짝수 합 구하기
+
+#include <stdio.h>
+int main(void)  {
+    int n;
+    int sum = 0;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++) {
+        if(i%2==0) {
+            sum = sum + i;
+        }
+    }
+    printf("%d\n", sum);
+    return 0;
+}
