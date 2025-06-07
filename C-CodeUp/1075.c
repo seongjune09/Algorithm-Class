6045076647d0f058ec59ba27437595a9ca6f25b2
박성준 <freestarjun@gmail.com>
2025-06-07 23:15:48 +0900

    2025/6/7/(토)

--- C-CodeUp/1075.c
+++ C-CodeUp/1075.c
@@ -1,1 +1,14 @@
 // 정수 1개 입력받아 카운트다운 출력하기
+
+#include <stdio.h>
+int main(void)  {
+    int n;
+    scanf("%d", &n);
+    for(int i=n-1; i>=0; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
+
+
--- C-CodeUp/1076.c
+++ C-CodeUp/1076.c
@@ -1,1 +1,12 @@
-//
+// 문자 1개 입력받아 알파벳 출력하기
+
+#include <stdio.h>
+int main(void)  {
+    char n;
+    scanf("%c", &n);
+    for(char c='a'; c <= n; c++) {
+        printf("%c ", c);
+    }
+
+    return 0;
+}
