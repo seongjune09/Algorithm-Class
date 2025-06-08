3337f0d11b603d67963f6050e97f557cc081b7fa
박성준 <freestarjun@gmail.com>
2025-06-08 23:37:03 +0900

    2025/6/8/(일)

--- Programming-A/2025-5-27/5-27(3x3 대칭 판별).c
+++ Programming-A/2025-5-27/5-27(3x3 대칭 판별).c
@@ -1,4 +1,21 @@
+// 3X3배열의 대칭 판별
+
 #include <stdio.h>
 int main(void) {
-
+    int arr[3][3];
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++) {
+            if(arr[i][j] != arr[j][i]) {
+                printf("대칭이 아닙니다.");
+                return 0;
+            }
+        }
+    }
+    printf("대칭입니다.");
+    return 0;
 }
