f378e6b9b8af0a1d784eeedae1788e935f71e484
박성준 <freestarjun@gmail.com>
2025-06-08 12:15:57 +0900

    2025/6/8/(일)

--- C-CodeUp/1082.c
+++ C-CodeUp/1082.c
@@ -1,1 +1,13 @@
-//
+// 16진수 구구단?
+
+#include <stdio.h>
+int main(void)  {
+    char c;
+    int a;
+    scanf("%X", &a);
+    for(int i=1; i<=15; i++) {
+        printf("%X*%X=%X\n", a,i, a*i);
+    }
+
+    return 0;
+}
--- C-CodeUp/1083.c
+++ C-CodeUp/1083.c
@@ -1,1 +1,17 @@
-//
+// 3 6 9 게임의 왕이 되자!
+
+#include <stdio.h>
+int main(void) {
+    int n;
+    char sum;
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++) {
+        if(i%3==0) {
+            printf("X ");
+        }
+        else {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
