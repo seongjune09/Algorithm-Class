127833f3cd72981a97d7fde3e8256b57cb8fbddf
박성준 <freestarjun@gmail.com>
2025-06-09 08:15:05 +0900

    2025/6/9/(월)

--- Programming-A/2025-5-27/5-27(이차원 배열 전체합의 역순 출력).c
+++ Programming-A/2025-5-27/5-27(이차원 배열 전체합의 역순 출력).c
@@ -1,20 +1,19 @@
-// 이차원배열 전체합의 역순 출력
+// 2x3이차원배열의 역순 출력
 
 #include <stdio.h>
 int main(void) {
-    int arr[3][3];
+    int arr[2][3];
     int sum = 0 , sum2 = 0, sum3 = 0;
-    for(int i=0; i<3; i++) {
+    for(int i=0; i<2; i++) {
         for(int j=0; j<3; j++) {
             scanf("%d", &arr[i][j]);
         }
     }
-    for(int i=0; i<3; i++) {
-        for(int j=0; j<3; j++) {
-            sum = sum + arr[i][j];
+    for(int i=1; i>-1; i--) {
+        for(int j=2; j>-1; j--) {
+            printf("%d", arr[i][j]);
             
         }
     }
-    printf("%d", sum);
     return 0;
 }
