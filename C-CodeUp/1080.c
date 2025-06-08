7b633eea81afb6030fa28c5dcece787abbb572e6
박성준 <freestarjun@gmail.com>
2025-06-08 11:46:51 +0900

    2025/6/7/(토)

--- C-CodeUp/1080.c
+++ C-CodeUp/1080.c
@@ -1,1 +1,16 @@
-//
+// 언제까지 더해야 할까?
+
+#include <stdio.h>
+int main(void)  {
+    int n;
+    int sum = 0;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++) {
+        sum += i;
+        if(sum >= n) {
+            printf("%d", i);
+            break;
+        }
+    }
+    return 0;
+}
--- C-CodeUp/1081.c
+++ C-CodeUp/1081.c
@@ -1,1 +1,14 @@
-//
+// 주사위를 2개 던지면?
+
+#include <stdio.h>
+int main(void)  {
+    int n=3,m=6;
+    scanf("%d %d", &n,&m);
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            printf("%d %d\n", i,j);
+        }
+    }
+
+    return 0;
+}
