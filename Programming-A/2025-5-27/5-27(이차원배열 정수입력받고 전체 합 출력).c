f19072e72216bda0f44546e2340d998e3a8bf543
박성준 <freestarjun@gmail.com>
2025-06-09 06:22:24 +0900

    2025/6/9/(월)

--- Programming-A/2025-5-27/5-27(이차원배열 정수입력받고 전체 합 출력).c
+++ Programming-A/2025-5-27/5-27(이차원배열 정수입력받고 전체 합 출력).c
@@ -1,1 +1,20 @@
 // 이차원배열 정수입력받고 전체 합 출력
+
+#include <stdio.h>
+int main(void) {
+    int arr[3][3];
+    int sum = 0 , sum2 = 0, sum3 = 0;
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(int i=0; i<3; i++) {
+        for(int j=0; j<3; j++) {
+            sum = sum + arr[i][j];
+            
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
--- Programming-A/2025-5-27/5-27(이차원배열(2x3)정수 입력받고 출력).c
+++ Programming-A/2025-5-27/5-27(이차원배열(2x3)정수 입력받고 출력).c
@@ -1,1 +1,18 @@
 // 이차원배열 2x3 정수 입력받고 출력
+
+#include <stdio.h>
+int main(void) {
+    int arr[2][3];
+    int sum = 0 , sum2 = 0, sum3 = 0;
+    for(int i=0; i<2; i++) {
+        for(int j=0; j<3; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(int i=0; i<2; i++) {
+        for(int j=0; j<3; j++) {
+                printf("%d", arr[i][j]);
+        }
+    }
+    return 0;
+}
