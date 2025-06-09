4485237573480b27908e0e16668d94b719a12a94
박성준 <freestarjun@gmail.com>
2025-06-09 11:56:05 +0900

    2025/6/9/(월)

--- C-CodeUp/1086.c
+++ C-CodeUp/1086.c
@@ -1,1 +1,11 @@
 // 그림 파일 저장용량 계산하기
+
+#include <stdio.h>
+int main(void) {
+    int w,h,b;
+    scanf("%d %d %d", &w,&h,&b);
+    
+    double size = (double)w*h*b / 8 / 1024 / 1024;
+    printf("%.2f MB", size);
+    return 0;
+}
