5894a18b25f9220217c89095e7689de6f00b8102
박성준 <freestarjun@gmail.com>
2025-06-14 19:15:04 +0900

    2025/6/14/(토)

--- C-CodeUp/1092.c
+++ C-CodeUp/1092.c
@@ -1,1 +1,13 @@
-// 
+// 함께 문제 푸는 날                
+
+#include <stdio.h>
+int main(void) {
+    long long a,b,c;
+    scanf("%lld %lld %lld", &a,&b,&c);
+    long long day = 1;
+    while (day%a!=0||day%b!=0||day%c!=0) {
+        day++;
+    }
+    printf("%lld", day);
+    return 0;
+}
