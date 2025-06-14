892cd46db31ebbc96ff620fe45396f68615b9948
박성준 <freestarjun@gmail.com>
2025-06-14 18:41:54 +0900

    2025/6/14/(토)

--- C-CodeUp/1090.c
+++ C-CodeUp/1090.c
@@ -1,1 +1,13 @@
 // 수 나열하기2
+
+#include <stdio.h>
+int main(void) {
+    long long a,r,n;
+    scanf("%lld %lld %lld", &a,&r,&n);
+    long long sum = a;
+    for(int i=0; i<n-1; i++) {
+        sum = sum * r;
+    }
+    printf("%lld", sum);
+    return 0;
+}
