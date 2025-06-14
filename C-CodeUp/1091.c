088fbda61e27b3bed02518fc2d0a3a6e26f842b8
박성준 <freestarjun@gmail.com>
2025-06-14 18:59:04 +0900

    2025/6/14/(토)

--- C-CodeUp/1091.c
+++ C-CodeUp/1091.c
@@ -1,1 +1,13 @@
-// 
+// 수 나열하기3
+
+#include <stdio.h>
+int main(void) {
+    long long int a,m,d,n;
+    scanf("%lld %lld %lld %lld", &a,&m,&d,&n);
+    long long int result = a;
+    for(int i=0; i<n-1; i++) {
+        result = result*(m)+d;
+    }
+    printf("%lld", result);
+    return 0;
+}
