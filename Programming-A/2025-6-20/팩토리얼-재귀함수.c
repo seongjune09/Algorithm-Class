1a912efa27e45f6c882b167276d6d39d456650ba
박성준 <freestarjun@gmail.com>
2025-06-30 23:37:11 +0900

    2025/6/30/(월)

--- Programming-A/2025-6-20/팩토리얼-재귀함수.c
+++ Programming-A/2025-6-20/팩토리얼-재귀함수.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+int fibo(int n);
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    printf("%d", fibo(n));
+}
 
-int f(int n) 
+int fibo(int n)
 {
-    
+    if(n==0) return 0;
+    else if(n==1) return 1;
+    else return fibo(n-1) + fibo(n-2);
 }
