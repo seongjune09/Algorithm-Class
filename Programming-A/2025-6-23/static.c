55d8eed0c42242af06593c627a72a752057a29a8
박성준 <freestarjun@gmail.com>
2025-07-02 13:19:13 +0900

    2025/7/2/(수)

--- Programming-A/2025-6-23/static.c
+++ Programming-A/2025-6-23/static.c
@@ -4,8 +4,9 @@ int a = 10;
 void f() {
     static int a = 5; 
     //static은 한번만 초기화함으로 한번 더한 후 초기화 하지않고그대로 더한다
+ 
+    printf("%d\n", a);
     a++;
-    printf("%d", a);
 }
 int main(void) {
     f();
