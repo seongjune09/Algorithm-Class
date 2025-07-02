51ef431da1dde65fdb7d3015ef2c4be4cafdcfa0
박성준 <freestarjun@gmail.com>
2025-07-03 08:00:02 +0900

    2025/7/3/(목)

--- Programming-B/2025-7-2-(수)/탐색과 정렬/선택 정렬.py
+++ Programming-B/2025-7-2-(수)/탐색과 정렬/선택 정렬.py
@@ -1,14 +1,13 @@
 def sel_sort(a):
     n = len(a)
-    for i in range(0,n-2):
-        max_idx = i
+    for i in range(0, n-2):
+        min_idx = i
         for j in range(i+1, n):
-            if a[j] < a[max_idx]:
-                max_idx = j
-        a[i],a[max_idx] = a[max_idx],a[i]
+            if a[j] < a[min_idx]:
+                min_idx = j
+        a[i],a[min_idx] = a[min_idx],a[i]
     return a
-
+            
 d = [2,4,5,1,3]
 sel_sort(d)
 print(d)
-                
