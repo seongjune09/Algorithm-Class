4e133c81b91cee5ea4d5a49ad9d263c9b8da9045
박성준 <freestarjun@gmail.com>
2025-07-07 22:18:59 +0900

    2025/7/7/(월)

--- Programming-B/2025-7-2-(수)/재귀호출/02-최대공약수.py
+++ Programming-B/2025-7-2-(수)/재귀호출/02-최대공약수.py
@@ -4,7 +4,6 @@ def gcd(a,b):
         if a%i == 0 and b%i == 0:
             return i
         i = i - 1
-    
 print(gcd(1, 5))    # 1
 print(gcd(3, 6))    # 3
 print(gcd(60, 24))  # 12
---
+++ Python-CodeUp/기초 100제/6034.py
@@ -0,0 +1,5 @@
+# 정수 2개 입력받아 차 계산하기
+
+n,m = map(int, input().split(''))
+c = n-m
+print(c)
