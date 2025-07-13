135c1de2cb7a077a1dd92d5097f1308621b988c5
박성준 <freestarjun@gmail.com>
2025-07-14 08:57:56 +0900

    2025/7/14/(월)

--- Python-CodeUp/기초 100제/6043.py
+++ Python-CodeUp/기초 100제/6043.py
@@ -1,1 +1,4 @@
 # 실수 2개 입력받아 나눈 결과 계산하기
+
+n,m = map(float, input().split(" "))
+print(format(n/m, ".3f"))
---
+++ Python-CodeUp/기초 100제/6044.py
@@ -0,0 +1,10 @@
+# 정수 2개 입력받아 자동 계산하기
+
+n,m = map(int, input().split())
+
+print(n+m)
+print(n-m)
+print(n*m)
+print(n//m)
+print(n%m)
+print(format(n/m, ".2f"))
