20f771b98a2d8abefc39267660ea0a53c3a8de6d
박성준 <freestarjun@gmail.com>
2025-08-29 06:24:59 +0900

    2025/8/29/(금)

--- Python-CodeUp/4-1.단순반복문/1254.py
+++
@@ -1,3 +0,0 @@
-n,m = input().split()
-for i in range(ord(n), ord(m) + 1):
-    print(chr(i), end=" ")
--- Python-CodeUp/4-1.단순반복문/1255.py
+++
@@ -1,7 +0,0 @@
-n,m = map(float,  input().split())
-
-a = round(n * 100)
-b = round(m * 100)
-
-for i in range(a,b+1):
-    print(f"{i / 100:.2f}", end=" ")
