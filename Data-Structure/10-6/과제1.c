731db328d8d844df54b4348c52a31e97ed5ca413
박성준 <freestarjun@gmail.com>
2025-10-07 22:23:39 +0900

    2025/10/7/(화)

--- Data-Structure/10-6/과제1.c
+++ Data-Structure/10-6/과제1.c
@@ -1,3 +1,25 @@
 // 1. 노드의 개수를 입력받고 연결리스트 생성, 데이터 입력받아서 저장, 정수 입력받고 입력 받은 수가 저장되어 있는 노드 위치 출력
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct Node
+{
+    element date;
+    struct Node *next;
+} Node;
+
+int main(void)
+{
+
+    int n, value;
+    Node *head = NULL;
+    Node *next = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        Node *new = (Node *)malloc(sizeof(Node));
+    }
+}
