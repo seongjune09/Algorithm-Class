2aa510b98985d4ab751a3e23c9b385c483727d22
박성준 <freestarjun@gmail.com>
2025-10-12 22:50:06 +0900

    2025/10/12/(일)

--- Data-Structure/10-6/과제3.c
+++ Data-Structure/10-6/과제3.c
@@ -1,1 +1,53 @@
 // 3. 노드의 개수를 입력받고 연결리스트 생성, 데이터 입력받아서 저장, 1을 입력받으면 제일 처음에 삽입, 2를 입력받으면 연결리스트의 정가운데 삽입
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct Node
+{
+    element data;
+    struct Node *next;
+} Node;
+
+int main(void)
+{
+    int n, value;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+        Node *new = (Node *)malloc(sizeof(Node));
+
+        new->data = value;
+        new->next = NULL;
+
+        if (head != NULL)
+        {
+            tail->next = new;
+            tail = new;
+        }
+        else
+        {
+            head = new;
+            tail = new;
+        }
+
+        int s;
+        scanf("%d", &s);
+        scanf("%d", &value);
+
+        if (s == 1)
+        {
+            new->next = head;
+            head = new;
+        }
+        else if (s == 2)
+        {
+        }
+    }
+}
