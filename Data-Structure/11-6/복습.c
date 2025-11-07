48efd326d0c8e4c665ec9548ec6a4db7f73aa5a6
박성준 <freestarjun@gmail.com>
2025-11-07 23:26:18 +0900

    2025/1/7/(금)

--- Data-Structure/11-6/복습.c
+++ Data-Structure/11-6/복습.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef int element;
 typedef struct Node
 {
-    element data;
-    struct Node *new;
+    int data;
+    struct Node *next;
 } Node;
 
 int main(void)
 {
+
     Node *head = NULL;
     Node *tail = NULL;
+
+    int n, value;
+    scanf("%", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &value);
+
+        Node *new(Node *) malloc(sizeof(Node));
+    }
 }
