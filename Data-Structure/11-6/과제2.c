c015c3ac8fd57dfd0070016036bf8705ad80dca1
박성준 <freestarjun@gmail.com>
2025-11-13 23:24:29 +0900

    2025/11/13(목)

--- Data-Structure/11-6/과제2.c
+++ Data-Structure/11-6/과제2.c
@@ -29,7 +29,7 @@ int main(void)
     Node *node[n]; // 노드 포인트 배열 사용 n개의 배열 생성
     for (int i = 0; i < n; i++)
     {
-        node[i] = (Node *)malloc(sizeof(Node)); // i라고 가정하고 동적메모리 생성
+        node[i] = (Node *)malloc(sizeof(Node)); // 각 배열에 동적메모리로 노드 구조체 생성
         scanf("%d", &node[i]->data);            // i++ 되면서 i자리에 데이터 값 넣어주기
         node[i]->left = NULL;                   // 링리 처럼 처음 left와 right는 널값
         node[i]->right = NULL;
