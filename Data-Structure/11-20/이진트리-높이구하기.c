b17806d54357c93073c1483fff327d72a47885ce
박성준 <freestarjun@gmail.com>
2025-11-27 05:46:18 +0900

    2025/11/27/(목)

--- Data-Structure/11-20/이진트리-높이구하기.c
+++ Data-Structure/11-20/이진트리-높이구하기.c
@@ -1,1 +1,60 @@
 // 3. BST 사용자 입력 받고 삽입, 삭제 후 높이 구하기
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct Node
+{
+    element data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+Node *new(element data)
+{
+    Node *new = (Node *)malloc(sizeof(Node));
+    new->data = data;
+    new->left = NULL;
+    new->right = NULL;
+    return new;
+}
+
+// 삽입 하는 함수
+
+// 삭제하는 함수
+
+// 트리 높이 계산 함수
+int height(Node *root)
+{
+    if (root == NULL)
+        return -1;
+
+    int leftH = height(root->left);
+    int rightH = height(root->right);
+
+    return (leftH > rightH ? leftH : rightH) + 1;
+}
+
+// 중위 순회 출력 함수
+void printTree(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    printTree(root->left);
+    printf("%d ", root->data);
+    printTree(root->right);
+}
+
+int main()
+{
+    Node *root = NULL;
+    int n;
+
+    printTree(root);
+    printf("\n트리 높이: %d\n", height(root));
+
+    return 0;
+}
--- Data-Structure/11-20/이진트리-삭제.c
+++ Data-Structure/11-20/이진트리-삭제.c
@@ -1,1 +1,40 @@
 //  BST 삭제
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct Node
+{
+    element data;
+    struct Node *left;
+    struct Node *right;
+} Node;
+
+void printTree(Node *root)
+{
+    if (root == NULL)
+        return;
+    printTree(root->left);
+    printf("%d ", root->data);
+    printTree(root->right);
+}
+
+int main()
+{
+    Node node5 = {60, NULL, NULL};
+    Node node4 = {12, NULL, NULL};
+    Node node3 = {9, NULL, NULL};
+    Node node2 = {3, NULL, NULL};
+    Node node1 = {8, &node2, &node3};
+    Node node0 = {10, &node1, &node4};
+    Node node_root = {25, &node0, &node5};
+
+    // 3삭제
+    node1.left = NULL; // 부모 node1에서 3 연결 제거
+    free(&node2);
+
+    printTree(&node_root);
+
+    return 0;
+}
