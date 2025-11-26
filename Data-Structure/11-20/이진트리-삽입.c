75416906f714a6e9bc4666d65bb0a1facb6b12e2
박성준 <freestarjun@gmail.com>
2025-11-27 00:17:58 +0900

    2025/11/27/(목)

--- Data-Structure/11-20/이진트리-삽입.c
+++ Data-Structure/11-20/이진트리-삽입.c
@@ -11,49 +11,47 @@ typedef struct Node
     struct Node *right;
 } Node;
 
-Node *find(Node *root, int key)
+void printTree(Node *root)
 {
     if (root == NULL)
-    {
-        return NULL;
-    }
-
-    if (root->data == key)
-        return root;
-    else
-        return (Node *)NULL;
+        return;
+    printTree(root->left);
+    printf("%d ", root->data);
+    printTree(root->right);
 }
 
-Node *LorRight(Node *root, int key)
+int main()
 {
-    if (root->data < key)
-        return root->right;
-    else
-        return root->left;
-}
+    Node node5 = {60, NULL, NULL};
+    Node node4 = {12, NULL, NULL};
+    Node node3 = {9, NULL, NULL};
+    Node node2 = {3, NULL, NULL};
+    Node node1 = {8, &node2, &node3};
+    Node node0 = {10, &node1, &node4};
+    Node node_root = {25, &node0, &node5};
 
-Node N7 = {70, NULL, NULL};
-Node N6 = {60, NULL, &N7};
-Node N5 = {50, NULL, &N6};
-Node N4 = {40, NULL, &N5};
-Node N3 = {30, NULL, &N4};
-Node N2 = {20, NULL, &N3};
-Node N1 = {10, NULL, &N2};
+    // 새로 삽입할 노드(값 4)
+    Node *new = (Node *)malloc(sizeof(Node));
+    new->data = 4;
+    new->left = NULL;
+    new->right = NULL;
 
-int main(void)
-{
-    int key;
-    Node *node = &N1;
-    scanf("%d", &key);
-    while (find(node, key) == NULL)
+    // BST 규칙에 따라 값 4 삽입
+    if (4 < node_root.data) // 4 < 25
     {
-        if (node == NULL)
+        if (4 < node0.data) // 4 < 10
         {
-            printf("didn't find");
+            if (4 < node1.data) // 4 < 8
+            {
+                if (4 > node2.data) // 4 > 3 오른쪽 삽입
+                {
+                    node2.right = new;
+                }
+            }
         }
-
-        node = LorRight(node, key);
     }
-    printf("%d", find(node, key)->data);
+
+    printTree(&node_root);
+
     return 0;
 }
