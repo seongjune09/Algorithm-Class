bf47cdb2b2a98f007521b8c30f7bfad8e7403665
박성준 <freestarjun@gmail.com>
2025-12-15 23:35:33 +0900

    2025/12/15(월)

---
+++ Data-Structure/시험모음.c/연결리스트역순출력.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+
+typedef struct Node
+{
+    element data;
+    struct Node *next;
+} Node;
+
+Node *reverse(Node *head)
+{
+    Node *prev = NULL;
+    Node *current = head;
+    Node *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+
+void printList(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
+}
+
+// int main(void)
+// {
+//     int n;
+//     scanf("%d", &n);
+
+//     Node *head = NULL;
+//     Node *temp = NULL;
+//     Node *end = NULL;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         temp = (Node *)malloc(sizeof(Node));
+//         scanf("%d", &temp->data);
+//         temp->next = NULL;
+
+//         if (head == NULL)
+//         {
+//             head = temp;
+//             end = temp;
+//         }
+//         else
+//         {
+//             end->next = temp;
+//             end = temp;
+//         }
+//     }
+
+//     head = reverse(head);
+//     printList(head);
+
+//     temp = head;
+//     while (temp != NULL)
+//     {
+//         Node *next = temp->next;
+//         free(temp);
+//         temp = next;
+//     }
+//     return 0;
+// }
---
+++ Data-Structure/시험모음.c/이진탐색트리(삭제).c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Tree
+{
+    int data;
+    struct Tree *left;
+    struct Tree *right;
+} T;
+
+T *create(int key)
+{
+    T *node = (T *)malloc(sizeof(T));
+    node->data = key;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+};
+
+T *serach_min(T *n)
+{
+    while (n->left != NULL)
+    {
+        n = n->left;
+    }
+    return n;
+}
+
+T *Delete(T *n, int key)
+{
+    if (n == NULL)
+        return NULL;
+
+    if (n->data < key)
+    {
+        n->right = Delete(n->right, key);
+    }
+    else if (n->data > key)
+    {
+        n->left = Delete(n->left, key);
+    }
+    else
+    {
+        if (n->left == NULL)
+        {
+            T *c = n->right;
+            free(n);
+            return c;
+        }
+        else if (n->right == NULL)
+        {
+            T *temp = n->left;
+            free(n);
+            return temp;
+        }
+        T *t = search_min(n->right);
+        n->data = t->data;
+        n->right = Delete(n->right, t->data);
+    }
+    return n;
+}
+
+// void inorder(T *root)
+// {
+//     if (root == NULL)
+//         return;
+
+//     inorder(root->left);
+//     printf("%d ", root->data);
+//     inorder(root->right);
+// }
+
+// int getLevel(T *n)
+// {
+//     if (n == NULL)
+//     {
+//         return 0;
+//     }
+//     int left = getLevel(n->left);
+//     int right = getLevel(n->right);
+
+//     return 1 + (left > right ? left : right);
+// }
+
+// int getParent(T *A, T *B, T *n)
+// {
+
+//     if ((A->data > n->data && B->data < n->data) || (A->data < n->data && B->data > n->data))
+//     {
+//         return n->data;
+//     }
+
+//     if (A->data < n->data && B->data < n->data)
+//     {
+//         getParent(A, B, n->left);
+//     }
+//     else if (A->data > n->data && B->data > n->data)
+//     {
+//         getParent(A, B, n->right);
+//     }
+// }
+
+// int main(void)
+// {
+
+//     T *node_root = insert_jaegui(NULL, 25);
+//     insert_jaegui(node_root, 10);
+//     insert_jaegui(node_root, 8);
+//     insert_jaegui(node_root, 3);
+//     insert_jaegui(node_root, 9);
+//     insert_jaegui(node_root, 12);
+//     Delete(node_root, 10);
+//     insert_jaegui(node_root, 60);
+
+//     inorder(node_root);
+
+//     int key;
+//     // scanf("%d",&key);
+
+//     // T * delete_node = Delete(node_root,key);
+
+//     inorder(node_root);
+
+//     printf("\n 레벨 : %d\n", getLevel(node_root));
+
+//     // printf("\n");
+//     // printf("%d",getParent(node_root->right, node_root->left->left,node_root));
+// }
